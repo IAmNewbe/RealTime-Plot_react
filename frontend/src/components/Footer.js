97e4d08d7d551f46f3ac55e7809dca45b18df451
IAmNewbe <ahlulhikam2222@gmail.com>
2023-06-11 03:08:05 +0700

    init

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center">
+            Crustea.id
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
